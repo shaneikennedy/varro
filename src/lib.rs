f3896e8bbfa04bb7f1c515fd8e7ad30cafe1f2fe
Shane Kennedy <shane.kennedy19@gmail.com>
2025-08-27 22:33:10 +0200

    Fix normalize, floats idiot

--- src/lib.rs
+++ src/lib.rs
@@ -240,14 +240,20 @@ impl Segment {
         }
     }
 
+    // For a segment, update the existing term_index with all
+    // the terms and corresponding TFs from DocumentSegment
+    // if the term doesn't already exist in the term index, insert a new one
     fn add_docucment_segment(&mut self, seg: &DocumentSegment) {
-        for (term, term_count) in seg.terms.iter() {
-            let mut tfdf = Tfdf::new(term);
-            tfdf.add_for_doc(seg);
-            self.term_index
-                .entry(term.to_string())
-                .and_modify(|t| t.term_freq.push((seg.document_id(), *term_count)))
-                .or_insert(tfdf);
+        for (term, _) in seg.terms.iter() {
+            if self.term_index.contains_key(term) {
+                self.term_index
+                    .entry(term.to_string())
+                    .and_modify(|t| t.add_for_doc(seg));
+            } else {
+                let mut tfdf = Tfdf::new(term);
+                tfdf.add_for_doc(seg);
+                self.term_index.insert(term.to_string(), tfdf);
+            }
         }
     }
 }
@@ -260,7 +266,7 @@ struct Tfdf {
 
     // Each tuple is a document_id, and the normalized fresquency
     // for the term in this doc, that is, # occurances / total words in the doc
-    term_freq: Vec<(String, i32)>,
+    term_freq: Vec<(String, f64)>,
     doc_freq: i32,
 }
 
@@ -276,7 +282,7 @@ impl Tfdf {
     pub fn add_for_doc(&mut self, doc_seg: &DocumentSegment) {
         self.term_freq.push((
             doc_seg.document_id(),
-            *doc_seg.terms.get(&self.term).unwrap() / doc_seg.document_length, // Normalize the TF by the document length
+            *doc_seg.terms.get(&self.term).unwrap() as f64 / doc_seg.document_length as f64, // Normalize the TF by the document length
         ));
         self.doc_freq += 1;
     }
