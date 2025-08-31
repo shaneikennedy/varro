583918347bf51cedafff8551945e7c71f23933dd
Shane Kennedy <shane.kennedy19@gmail.com>
2025-08-31 13:36:19 +0200

    Make all examples have a debug log level

--- examples/document.rs
+++ examples/document.rs
@@ -1,6 +1,8 @@
 use log::{LevelFilter, info};
 
 fn main() {
-    env_logger::builder().filter_level(LevelFilter::Info).init();
+    env_logger::builder()
+        .filter_level(LevelFilter::Debug)
+        .init();
     info!("Document: {}", varro::Document::new().id());
 }
