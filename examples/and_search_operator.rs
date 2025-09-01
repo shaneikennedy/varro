45a42e619af320832ceb5be964d5b109b6e97461
Shane Kennedy <shane.kennedy19@gmail.com>
2025-09-02 00:04:00 +0200

    Optional AND type queries
    You can now specify if you want the search results to use AND or OR
    when matching against terms in the query. Default is OR.

---
+++ examples/and_search_operator.rs
@@ -0,0 +1,26 @@
+use std::path::Path;
+
+use anyhow::Result;
+use log::{LevelFilter, warn};
+use varro::{SearchOperator, SearchOptions, Varro};
+
+fn main() -> Result<()> {
+    env_logger::builder()
+        .filter_level(LevelFilter::Debug)
+        .init();
+
+    let search_engine = Varro::new(Path::new("./.index"))?;
+    if search_engine.index_size() == 0 {
+        warn!("There are no documents in the index, try running the ingest exmaple first");
+    }
+    let opts = SearchOptions::new()
+        .include_documents(true)
+        .search_operator(SearchOperator::AND);
+    let results = search_engine.search("git gorilla".into(), Some(opts));
+    assert_eq!(
+        results.count(),
+        0,
+        "surely there are no docs that talk about both git AND gorillas"
+    );
+    Ok(())
+}
