0a176b699f209349f1a81df416b257a2a33552a3
Shane Kennedy <shane.kennedy19@gmail.com>
2025-09-13 21:56:20 +0000

    🧪 Pyvarro-2 (#pyvarro-2)
    Summary: Added Python bindings for `Document` and `Field` with methods for creation and manipulation, and implemented an index flush function.
    
    Highlights:
    
    • Introduced `Document` and `Field` classes with methods like `new`, `add_field`, and `size`.
    • Added `flush` method to `PyVarro` for writing indexes to disk.
    • Implemented `Field::new` in `document.rs` for field initialization.
    
    Read more: https://pierre.co/shaneikennedy/varro/pyvarro-2

--- pyvarro/src/lib.rs
+++ pyvarro/src/lib.rs
@@ -16,6 +16,49 @@ pub struct Document {
 }
 
 #[pymethods]
+impl Document {
+    #[new]
+    pub fn new(id: String) -> Self {
+        Self {
+            doc: varro::Document::new(id),
+        }
+    }
+
+    /// Add a field to the document
+    pub fn add_field(&mut self, name: String, contents: String, index: bool) {
+        self.doc.add_field(name, contents, index);
+    }
+
+    /// Return the number of bytes allocated by a document
+    pub fn size(&self) -> usize {
+        self.doc.size()
+    }
+}
+
+/// The model representing a field in a document
+#[pyclass]
+#[derive(Clone)]
+pub struct Field {
+    field: varro::Field,
+}
+
+#[pymethods]
+impl Field {
+    #[new]
+    pub fn new(name: &str, contents: &str) -> PyResult<Field> {
+        Ok(Field {
+            field: varro::Field::new(name, contents),
+        })
+    }
+    pub fn name(&self) -> String {
+        self.field.name()
+    }
+    pub fn contents(&self) -> String {
+        self.field.contents()
+    }
+}
+
+#[pymethods]
 impl PyVarro {
     #[new]
     pub fn new(
@@ -45,5 +88,11 @@ impl PyVarro {
     /// The total number of docs in the Varro index
     pub fn index_size(&self) -> usize {
         self.varro.index_size()
+    }
+
+    /// Flush the indexes to disk, this needs to happen before a document is searchable
+    pub fn flush(&self) -> PyResult<()> {
+        self.varro.flush().unwrap();
+        Ok(())
     }
 }
