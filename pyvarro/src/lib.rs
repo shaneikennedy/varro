fcc63ea94ade63e8d64182cfb4e4eb71354fcdea
Shane Kennedy <shane.kennedy19@gmail.com>
2025-09-14 11:52:07 +0200

    Splitting in pyo3 is complicated, move everything into lib.rs

--- pyvarro/src/lib.rs
+++ pyvarro/src/lib.rs
@@ -1,14 +1,135 @@
-mod document;
-mod search;
-
 #[pyo3::pymodule]
 mod pyvarro {
-    use crate::document::Document;
-    use crate::search::SearchOptions;
     use pyo3::prelude::*;
     use std::path::Path;
     use std::time::Duration;
     use varro::Varro;
+
+    #[pyclass]
+    #[derive(Clone)]
+    pub struct Document {
+        pub(crate) doc: varro::Document,
+    }
+
+    #[pymethods]
+    impl Document {
+        #[new]
+        pub fn new(id: String) -> Self {
+            Self {
+                doc: varro::Document::new(id),
+            }
+        }
+
+        /// Add a field to the document
+        pub fn add_field(&mut self, name: String, contents: String, index: bool) {
+            self.doc.add_field(name, contents, index);
+        }
+
+        /// Return the number of bytes allocated by a document
+        pub fn size(&self) -> usize {
+            self.doc.size()
+        }
+    }
+
+    impl From<varro::Document> for Document {
+        fn from(value: varro::Document) -> Self {
+            let mut doc = Document::new(value.id());
+            for field in value.fields() {
+                doc.add_field(field.name(), field.contents(), false);
+            }
+            doc
+        }
+    }
+
+    /// The model representing a field in a document
+    #[pyclass]
+    #[derive(Clone)]
+    pub struct Field {
+        pub(crate) field: varro::Field,
+    }
+
+    #[pymethods]
+    impl Field {
+        #[new]
+        pub fn new(name: &str, contents: &str) -> PyResult<Field> {
+            Ok(Field {
+                field: varro::Field::new(name, contents),
+            })
+        }
+        pub fn name(&self) -> String {
+            self.field.name()
+        }
+        pub fn contents(&self) -> String {
+            self.field.contents()
+        }
+    }
+
+    #[pyclass]
+    #[derive(Clone)]
+    pub enum SearchOperator {
+        Or,
+        And,
+    }
+
+    impl From<SearchOperator> for varro::SearchOperator {
+        fn from(val: SearchOperator) -> varro::SearchOperator {
+            match val {
+                SearchOperator::Or => varro::SearchOperator::OR,
+                SearchOperator::And => varro::SearchOperator::AND,
+            }
+        }
+    }
+
+    #[pyclass]
+    #[derive(Clone)]
+    pub enum RankingType {
+        Tfidf,
+        Bm25,
+    }
+
+    impl From<RankingType> for varro::RankingType {
+        fn from(val: RankingType) -> varro::RankingType {
+            match val {
+                RankingType::Tfidf => varro::RankingType::Tfidf,
+                RankingType::Bm25 => varro::RankingType::Bm25,
+            }
+        }
+    }
+
+    #[derive(Clone)]
+    #[pyclass]
+    #[allow(dead_code)]
+    pub struct SearchOptions {
+        options: varro::SearchOptions,
+    }
+
+    impl From<SearchOptions> for varro::SearchOptions {
+        fn from(val: SearchOptions) -> varro::SearchOptions {
+            val.options
+        }
+    }
+
+    #[pymethods]
+    impl SearchOptions {
+        #[new]
+        pub fn new(
+            include_documents: Option<bool>,
+            operator: Option<SearchOperator>,
+            ranking_type: Option<RankingType>,
+        ) -> SearchOptions {
+            let mut opts = varro::SearchOptions::default();
+            if let Some(include_documents) = include_documents {
+                opts = opts.with_include_documents(include_documents);
+            }
+            if let Some(operator) = operator {
+                opts = opts.with_search_operator(operator.into());
+            }
+            if let Some(ranking_type) = ranking_type {
+                opts = opts.with_ranking_type(ranking_type.into());
+            }
+            SearchOptions { options: opts }
+        }
+    }
 
     #[pyclass]
     pub struct PyVarro {
@@ -27,7 +148,7 @@ mod pyvarro {
             compaction_frequency: Option<Duration>,
             max_buffer_size: Option<usize>,
         ) -> PyResult<PyVarro> {
-            let mut varro = Varro::new(Path::new("./index")).unwrap();
+            let mut varro = Varro::new(Path::new(".index")).unwrap();
             if let Some(min_segment_size) = min_segment_size {
                 varro = varro.with_min_segment_size(min_segment_size);
             }
