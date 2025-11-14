1037fa82db21bb7c6e468d3cded41218b7c8f2d5
Shane Kennedy <shane.kennedy19@gmail.com>
2025-11-14 15:52:17 +0100

    Move compaction fields into compactor

--- lib/src/manifest.rs
+++ lib/src/manifest.rs
@@ -7,11 +7,21 @@ pub type SegmentSize = usize;
 #[derive(Encode, Decode, Debug)]
 pub(crate) struct Manifest {
     /// The active segments in the index used for searching.
-    pub segments: HashMap<SegmentId, SegmentSize>,
+    pub(crate) segments: HashMap<SegmentId, SegmentSize>,
 
     /// The total number of docs in the index.
-    pub total_docs: usize,
+    pub(crate) total_docs: usize,
 
     /// Average document length in the index.
-    pub average_document_length: f64,
+    pub(crate) average_document_length: f64,
+}
+
+impl Manifest {
+    pub(crate) fn new() -> Self {
+        Self {
+            segments: HashMap::new(),
+            total_docs: 0,
+            average_document_length: 0.0,
+        }
+    }
 }
